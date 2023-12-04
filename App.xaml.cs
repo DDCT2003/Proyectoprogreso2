1be493ab97dbf117312a9527a76462efe81076c3
DDCT2003 <dylanclerque_2003@hotmail.com>
2023-12-04 07:42:38 -0500

    Primer commit:)

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Proyectoprogreso2
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
